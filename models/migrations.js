054108743bb7df3ffe3341c030b48f9f015041f4
danschumann <dan.schumann@gmail.com>
2014-04-10 20:13:28 -0500

    removed base app from migrations
    base app being added as limb

--- models/migrations.js
+++ models/migrations.js
@@ -99,7 +99,6 @@ module.exports = function(limby) {
       // their migrations might depend on previous limby migrations
       // so we have to combine all migrations into 1 list and run in order
       var limbyMigrationsPath = join(__dirname, '..', 'migrations');
-      var appMigrationsPath = join(limby._limbs, '..', 'migrations');
 
       return when(fs.readdir(limbyMigrationsPath)).then(function(limbyMigrationFiles){
 
@@ -108,15 +107,7 @@ module.exports = function(limby) {
           files.push([fileName, join(limbyMigrationsPath, fileName)])
         })
 
-        return fs.exists(appMigrationsPath).then(function(exists){
-          if (exists)
-            return fs.readdir(appMigrationsPath);
-        });
       }).then(function(appMigrationFiles){
-        _.each(appMigrationFiles, function(fileName){
-          // We will sort based on fileName so we keep that separate, but then we will need the full path
-          files.push([fileName, join(appMigrationsPath, fileName)])
-        })
 
         _.each(limby.limbs, function(branch, branchName){
           _.each(branch.migrations, function(migration, fileName){
