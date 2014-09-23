94ff80e49723838c5eb6018d126b0e229edc564a
Dan Schumann <dan.schumann@gmail.com>
2014-09-23 13:28:56 -0500

    fixed signup, showing errors if there are code errors

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,6 +2,7 @@ module.exports = function(limby, models) {
 
   var
     _     = require('underscore'),
+    debug = require('debug')('limby:controllers:signup'),
     User  = models.User;
 
   var controller = {
@@ -11,6 +12,8 @@ module.exports = function(limby, models) {
     },
 
     post: function(req, res, next) {
+
+      debug('post');
 
       // Could have been supplied by middleware
       var user = req.locals.user || User.forge();
@@ -23,28 +26,36 @@ module.exports = function(limby, models) {
 
       attributes.password = req.body.password;
 
+      debug('set, validate')
       return user
         .set(attributes)
         .validateBatch('signup')
         .then(function(){
+          debug('trigger signup')
           return user.trigger('signup') // any middleware supplied user could have callbacks
         })
         .then(function(){
+          debug('hashPassword')
           return user.hashPassword()
         })
         .then(function(){
+          debug('save')
           return user.save({method: 'insert'})
         })
         .then(function(){
+          debug('saved', user && user.toJSON())
           if (!user.id) throw new Error('Could not create user, unknown error!');
 
           // Success
           user.mailers && _.isFunction(user.mailers.signup) && user.mailers.signup();
+          console.log(user, user.get('id'));
+          debug('set session', user.get('id'));
           req.session.user_id = user.get('id');
           res.redirect(limby.baseURL + '/');
 
         })
-        .otherwise(function(){
+        .otherwise(function(er){
+          if (er) console.log('Signup error'.red, er, er.stack);
           req.flash.danger(user.errors);
 
           // Remember fields to put back into form
