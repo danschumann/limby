cbaa3f79c8ab0b200d3dc2d68759878eacd1276f
Dan Schumann <dan.schumann@gmail.com>
2014-10-30 11:57:10 -0500

    added controller for error support

---
+++ controllers/error.js
@@ -0,0 +1,19 @@
+module.exports = function(limby, models){
+
+  var
+    _        = require('underscore');
+
+  return function(err, req, res, next) {
+    console.log('Server Error!'.red, err, err.stack);
+    if (!(err instanceof Error)) err = new Error(err.toString());
+    limby.email({
+      to: limby.config.mail.webmaster,
+      from: limby.config.mail.from,
+      subject: limby.config.mail.errorSubject || 'Server Error',
+      text: err.toString() + '\n' + err.stack,
+      html: err.toString() + '<br />' + err.stack.toString().replace(/\n/g, '<br />'),
+    }).then(function(){
+      res.view('error');
+    });
+  };
+};
