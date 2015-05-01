aa5738d3965c350f80753edeba46517425a3cf64
Dan Schumann <dan.schumann@gmail.com>
2015-05-01 09:47:22 -0500

    stopped making an assumption that they wouldn't have a user when logging in

--- controllers/login.js
+++ controllers/login.js
@@ -12,7 +12,7 @@ module.exports = function(limby, models) {
 
     post: function(req, res, next) {
 
-      var user = req.locals.user || User.forge({
+      var user = User.forge({
         email: _.escape(req.body.email),
         password: req.body.password,
       });
